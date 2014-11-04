c3a1f66fd45a2d8bc7a8a2584716e331c038ecf3
a226035102 <comeonjiji@gmail.com>
2014-11-04 09:36:18 +0800

    Initial Commit

---
+++ JZRippleButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JZRippleButton
+//
+//  Created by Joshua Zhou on 14/11/4.
+//  Copyright (c) 2014年 Joshua Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JZRippleButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JZRippleButton
+//
+//  Created by Joshua Zhou on 14/11/4.
+//  Copyright (c) 2014年 Joshua Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
